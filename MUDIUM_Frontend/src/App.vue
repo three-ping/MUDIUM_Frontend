5fbe00d155bda712338cfe6e2fb6e27e326842f9
dongkh9 <dongkh99@gamil.com>
2024-10-11 12:46:40 +0900

    refactor: 프로젝트 구조 오류 수정
    초기 세팅 오류 수정

--- MUDIUM_Frontend/src/App.vue
+++ MUDIUM_Frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-
+    <h1>App.vue에서 보이는 메시지 입니다</h1>
+    <RouterView></RouterView>
   </div>
 </template>
 
--- MUDIUM_Frontend/src/router/index.js
+++ MUDIUM_Frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-// 도메인별 라우트 가져오기
 
 import HomeView from '@/views/HomeView.vue';
 
