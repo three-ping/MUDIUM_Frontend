2cfdd6f979b5962f5927c892f9fd95eb8a66b98c
1etterh <1etterh.dev@gmail.com>
2024-10-14 16:13:32 +0900

    feat: import @pico

--- MUDIUM_Frontend/src/main.js
+++ MUDIUM_Frontend/src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/index.js'; // 라우터 가져오기
-
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index.js"; // 라우터 가져오기
+import "@picocss/pico";
 const app = createApp(App);
 
 app.use(router); // 라우터 사용 등록
-app.mount('#app'); // Vue 애플리케이션을 마운트
+app.mount("#app"); // Vue 애플리케이션을 마운트
