38b15e00e351f4e75b12b0bfb0c8f45ecdf3644a
1etterh <1etterh.dev@gmail.com>
2024-10-16 01:10:31 +0900

    add: themeselector

--- MUDIUM_Frontend/src/App.vue
+++ MUDIUM_Frontend/src/App.vue
@@ -1,32 +1,7 @@
 <template>
+  <Navigation />
 
-  <nav>
-    <ul>
-      <li><img src="@/assets/Mudium_small.svg" alt=""></li>
-    </ul>
-    <ul>
-      <li><strong>뮤지컬</strong></li>
-      <li><strong>작품게시판</strong></li>
-      <li><strong>자유게시판</strong></li>
-      <li><strong>가이드북</strong></li>
-      <li><strong>공지사항</strong></li>
-    </ul>
-
-    <ul><input type="search" placeholder="검색어를 입력하세요"></ul>
-    <ul>
-      <li>
-        <details class="dropdown">
-          <summary>로그인</summary>
-          <ul dir="rtl">
-            <li><a href="#">로그인</a></li>
-            <li><a href="#">회원가입</a></li>
-          </ul>
-        </details>
-      </li>
-    </ul>
-    <hr>
-  </nav>
-  <main>
+  <main class=container>
     <RouterView />
   </main>
 </template>
@@ -35,6 +10,8 @@
 import { RouterView, RouterLink } from 'vue-router';
 
 import AppHeader from '@/components/AppHeader.vue';
+import Navigation from './components/Navigation.vue';
+
 </script>
 
 <style>
---
+++ MUDIUM_Frontend/src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+	<header class="container">
+		<nav>
+			<ul>
+				<li><img src="@/assets/Mudium_small.svg" alt=""></li>
+			</ul>
+			<ul>
+				<li><strong>뮤지컬</strong></li>
+				<li><strong>작품게시판</strong></li>
+				<li><strong>자유게시판</strong></li>
+				<li><strong>가이드북</strong></li>
+				<li><strong>공지사항</strong></li>
+			</ul>
+
+			<ul><input type="search" placeholder="검색어를 입력하세요"></ul>
+			<ul>
+				<li>
+					<details class="dropdown">
+						<summary>로그인</summary>
+						<ul dir="rtl">
+							<li><a href="#">로그인</a></li>
+							<li><a href="#">회원가입</a></li>
+						</ul>
+					</details>
+				</li>
+				<li>
+					<details class="dropdown" id="theme-selector">
+						<summary role="button" class="secondary">Theme</summary>
+						<ul>
+							<li><a href="#" data-theme-switcher="auto">Auto</a></li>
+							<li><a href="#" data-theme-switcher="light">Light</a></li>
+							<li><a href="#" data-theme-switcher="dark">Dark</a></li>
+						</ul>
+					</details>
+				</li>
+			</ul>
+			<hr>
+		</nav>
+	</header>
+
+</template>
+
+<script setup>
+import "@/scripts/minimal-theme-switcher.js"
+</script>
+
+<style lang="scss" scoped></style>
