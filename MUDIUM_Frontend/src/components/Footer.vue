0bd1617fc6360c7221fee64807d34c38b97ffee1
1etterh <1etterh.dev@gmail.com>
2024-10-16 05:30:48 +0900

    chore: pickTheme

--- MUDIUM_Frontend/src/components/Footer.vue
+++ MUDIUM_Frontend/src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
 	<footer>
+		<ToggleTheme />
 	</footer>
 </template>
 
 <script setup>
+import ToggleTheme from './ToggleTheme.vue';
 </script>
 
 <style scoped></style>
--- MUDIUM_Frontend/src/components/ToggleTheme.vue
+++ MUDIUM_Frontend/src/components/ToggleTheme.vue
@@ -1,10 +1,48 @@
 <template>
 	<div>
-
+		<button @click="pickTheme"></button>
 	</div>
 </template>
 
 <script setup>
+import { onMounted, reactive, watch } from 'vue';
+const state = reactive({ currentTheme: null, })
+
+
+const setTheme = (theme) => {
+	state.currentTheme = theme;
+}
+const updateMetaTheme = () => {
+	const metaTheme = document.querySelector('meta[name="color-scheme"]');
+	console.log(`metaTheme: ${metaTheme.tagName}`)
+	console.log(`currentTheme: ${state.currentTheme}`)
+	metaTheme.setAttribute('content', state.currentTheme);
+}
+
+const pickTheme = () => {
+	const newTheme = state.currentTheme === 'dark' ? 'light' : 'dark';
+
+	if (state.currentTheme !== newTheme) {
+		localStorage.setItem('theme', newTheme);
+		console.log(`newTheme: ${newTheme}`)
+		setTheme(newTheme);
+	}
+}
+
+onMounted(() => {
+	const savedTheme = localStorage.getItem('theme');
+	if (savedTheme) {
+		console.log(`savedTheme: ${savedTheme}`)
+		setTheme(savedTheme);
+	} else {
+		setTheme('light');
+	}
+});
+
+// watch(() => state.currentTheme, () => {
+// 	document.documentElement.setAttribute('dataTheme', state.currentTheme);
+// 	updateMetaTheme();
+// })
 
 </script>
 
