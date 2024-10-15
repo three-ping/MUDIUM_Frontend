dd27458990b0e66f68e96b08f88bb473c16b6604
1etterh <1etterh.dev@gmail.com>
2024-10-16 04:31:54 +0900

    feat: Navigation

--- MUDIUM_Frontend/src/components/Navigation.vue
+++ MUDIUM_Frontend/src/components/Navigation.vue
@@ -1,13 +1,19 @@
 <template>
-	<header>
+	<header class="container-fluid">
 		<nav>
 			<img src="@/assets/Mudium_small.svg" width="100px" alt="">
 			<ul>
 				<li>
-					<a href="#"><strong>뮤지컬</strong></a>
+					<a href="#" class="contrast"><strong>뮤지컬</strong></a>
 				</li>
-				<li><a href="#"><strong>작품게시판</strong></a></li>
-				<li><a href="#"><strong>자유게시판</strong></a></li>
+				<li><a href="#" class="contrast"><strong>작품게시판</strong></a></li>
+				<li><a href="#" class="contrast"><strong>자유게시판</strong></a></li>
+				<li><a href="#" class="contrast"><strong>가이드북</strong></a></li>
+			</ul>
+			<ul>
+				<li><input type="search"></li>
+				<li><a href="#" class="contrast">로그인</a></li>
+				<li><a href="#" class="contrast">회원가입</a></li>
 			</ul>
 		</nav>
 	</header>
