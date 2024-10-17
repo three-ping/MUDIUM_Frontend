5a3d306b75629908aa63aff9b2f55814809d951e
1etterh <1etterh.dev@gmail.com>
2024-10-17 16:43:18 +0900

    fix: Navigation에 검색 기능 적용

--- MUDIUM_Frontend/src/components/layout/Navigation.vue
+++ MUDIUM_Frontend/src/components/layout/Navigation.vue
@@ -38,7 +38,8 @@
 				</li>
 			</ul>
 			<ul>
-				<li><input type="search"></li>
+				<li><input type="search" v-model="searchQuery" placeholder="검색어를 입력하세요" @keyup.enter="performSearch">
+				</li>
 				<li v-if="!isLoggedIn">
 					<details class="dropdown">
 						<summary>계정</summary>
@@ -63,14 +64,27 @@
 <script setup>
 import LoginModal from '@/views/user/components/LoginModal.vue';
 import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 // 현재 경로를 추적하기 위한 변수
+
 const route = useRoute();
+const router = useRouter();
 const currentRoute = ref(route.path);
 
 let isLoginModalVisible = ref(false);
 const isLoggedIn = ref(false);
+const hasSearched = ref(false);
+const searchQuery = ref('');
+
+
+const performSearch = () => {
+	if (!searchQuery.value.trim()) return; // 검색어가 없으면 실행하지 않음
+	router.push({
+		path: '/musicalInfo',  // Search results will be shown here
+		query: { title: searchQuery.value }
+	});
+};
 // 경로가 변경될 때마다 currentRoute 업데이트
 watch(route, (newRoute) => {
 	currentRoute.value = newRoute.path;
