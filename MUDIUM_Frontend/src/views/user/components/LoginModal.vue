812617b0316a52e574b192790df6d42e3f1f6724
1etterh <1etterh.dev@gmail.com>
2024-10-18 15:26:54 +0900

    chore: responsetype 수정 전

--- MUDIUM_Frontend/src/views/user/components/LoginModal.vue
+++ MUDIUM_Frontend/src/views/user/components/LoginModal.vue
@@ -54,6 +54,17 @@ const loginNormalUser = async () => {
 	}
 };
 
+const loginKakaoUser = async () => {
+	try {
+
+		const kakaoRedirectUri = "http://localhost:8080/api/users/oauth2/kakao";
+		const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=454fe5e6a0e0c020cf155003e27761e2& redirect_uri=${kakaoRedirectUri}& response_type=code`;
+		window.location.href = kakaoLoginUrl;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
 
 
 const closeModal = () => {
