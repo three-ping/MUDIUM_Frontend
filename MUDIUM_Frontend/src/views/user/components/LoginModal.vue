a44afc6837085e300d99e82a6062b95af0ed4ed7
1etterh <1etterh.dev@gmail.com>
2024-10-19 23:51:26 +0900

    feat: 카카오 로그인 후 콜백

--- MUDIUM_Frontend/src/views/user/components/LoginModal.vue
+++ MUDIUM_Frontend/src/views/user/components/LoginModal.vue
@@ -1,22 +1,20 @@
 <template>
 	<Modal :isModalOpen="isLoginModalVisible" @close="closeLoginModal">
-		<template v-slot:modalSection>
+		<template #modalSection>
 			<div class="login-form">
 				<input type="text" v-model="email" placeholder="아이디" class="auth-input">
 				<input type="password" v-model="password" placeholder="비밀번호" class="auth-input">
 				<p v-if="loginError" class="error-message">{{ loginError }}</p>
 			</div>
 		</template>
-		<template v-slot:modalFooter>
-
+		<template #modalFooter>
 			<div class="login-buttons">
 				<button @click="loginNormalUser" class="btn-auth btn-login">로그인</button>
-				<button @click="loginKakaoUser" class="btn-auth btn-kakao">
+				<button @click="initiateKakaoLogin" class="btn-auth btn-kakao">
 					<img src="@/assets/images/kakao-icon.svg" alt="Kakao" class="kakao-icon">
 					카카오 로그인
 				</button>
 			</div>
-
 			<div class="auth-links">
 				<a href="#" @click.prevent="openSignup">회원가입</a>
 				<span class="separator">|</span>
@@ -29,9 +27,10 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import Modal from '@/components/layout/Modal.vue';
 import axios from 'axios';
-import { ref } from 'vue';
 
 const props = defineProps({
 	isLoginModalVisible: Boolean,
@@ -39,6 +38,7 @@ const props = defineProps({
 
 const emit = defineEmits(['update:isLoggedIn', 'close', 'update:userInfo']);
 
+const router = useRouter();
 const email = ref('');
 const password = ref('');
 const loginError = ref('');
@@ -46,14 +46,14 @@ const loginError = ref('');
 const loginNormalUser = async () => {
 	try {
 		loginError.value = '';
-		const response = await axios.post("http://localhost:5173/api/users/login", {
+		const response = await axios.post('/api/users/login', {
 			email: email.value,
 			password: password.value,
-			signup_path: "NORMAL"
+			signup_path: 'NORMAL'
 		});
 
 		if (response.data.success) {
-			console.log("Normal Login Success");
+			console.log('Normal Login Success');
 			emit('update:isLoggedIn', true);
 			emit('update:userInfo', response.data.data);
 			closeLoginModal();
@@ -61,20 +61,44 @@ const loginNormalUser = async () => {
 			loginError.value = '로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.';
 		}
 	} catch (error) {
-		console.error("Login error:", error);
+		console.error('Login error:', error);
 		loginError.value = '로그인 중 오류가 발생했습니다. 나중에 다시 시도해주세요.';
 	}
 };
 
-const loginKakaoUser = async () => {
-	try {
-		const kakaoRedirectUri = "http://127.0.0.1:8080/api/users/oauth2/kakao";
-		const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=454fe5e6a0e0c020cf155003e27761e2&redirect_uri=${kakaoRedirectUri}&response_type=code`;
-		window.location.href = kakaoLoginUrl;
-	} catch (error) {
-		console.log(error);
+const initiateKakaoLogin = () => {
+	const kakaoRedirectUri = `${window.location.origin}/kakao-callback`;
+	const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=454fe5e6a0e0c020cf155003e27761e2&redirect_uri=${encodeURIComponent(kakaoRedirectUri)}&response_type=code`;
+	window.location.href = kakaoLoginUrl;
+};
+
+const handleKakaoCallback = async () => {
+	const urlParams = new URLSearchParams(window.location.search);
+	const code = urlParams.get('code');
+
+	if (code) {
+		try {
+			const response = await axios.post(`/api/users/oauth2/kakao?code=${code}`);
+			if (response.data.success) {
+				console.log('Kakao Login Success');
+				emit('update:isLoggedIn', true);
+				emit('update:userInfo', response.data.data);
+				router.push('/'); // Redirect to home page or dashboard
+			} else {
+				loginError.value = '카카오 로그인에 실패했습니다. 다시 시도해주세요.';
+			}
+		} catch (error) {
+			console.error('Kakao login error:', error);
+			loginError.value = '카카오 로그인 중 오류가 발생했습니다. 나중에 다시 시도해주세요.';
+		}
 	}
 };
+
+onMounted(() => {
+	if (window.location.pathname === '/kakao-callback') {
+		handleKakaoCallback();
+	}
+});
 
 const closeLoginModal = () => {
 	emit('close');
@@ -95,7 +119,6 @@ const openFindPassword = () => {
 	console.log('Open find password');
 };
 </script>
-
 <style scoped>
 .login-form {
 	display: flex;
